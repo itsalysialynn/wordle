487fe875948c867bbbf42e389dea08de0020370a
Alysia <alysia@bench.co>
2023-09-24 15:15:10 -0700

    Improve tile testing

--- src/components/board/Tile.spec.tsx
+++ src/components/board/Tile.spec.tsx
@@ -8,6 +8,21 @@ describe('Tile', () => {
     expect(container.firstChild).toHaveStyleRule('border', '2px solid #d3d6da');
   });
 
+  it('renders a blank tile with a boarder when no letter is provided and validation is CORRECT', async () => {
+    const { container } = render(<Tile validation={TileValidation.CORRECT} />);
+    expect(container.firstChild).toHaveStyleRule('border', '2px solid #d3d6da');
+  });
+
+  it('renders a blank tile with a boarder when no letter is provided and validation is PRESENT', async () => {
+    const { container } = render(<Tile validation={TileValidation.PRESENT} />);
+    expect(container.firstChild).toHaveStyleRule('border', '2px solid #d3d6da');
+  });
+
+  it('renders a blank tile with a boarder when no letter is provided and validation is INCORRECT', async () => {
+    const { container } = render(<Tile validation={TileValidation.INCORRECT} />);
+    expect(container.firstChild).toHaveStyleRule('border', '2px solid #d3d6da');
+  });
+
   it('renders a green tile with when a letter is provided and validation is CORRECT', async () => {
     const { container } = render(<Tile letter="a" validation={TileValidation.CORRECT} />);
     expect(container.firstChild).toHaveStyleRule('border', '2px solid #6aaa64');
@@ -20,7 +35,7 @@ describe('Tile', () => {
     expect(container.firstChild).toHaveStyleRule('background-color', '#c9b458');
   });
 
-  it('renders a grey tile with when a letter is provided and validation is PRESENT', async () => {
+  it('renders a grey tile with when a letter is provided and validation is INCORRECT', async () => {
     const { container } = render(<Tile letter="a" validation={TileValidation.INCORRECT} />);
     expect(container.firstChild).toHaveStyleRule('border', '2px solid #787c7e');
     expect(container.firstChild).toHaveStyleRule('background-color', '#787c7e');
